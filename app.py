96d644d0e863a94c9811bb8a8348652c25ec4ebd
Tanya Da Costa <tanya@hifyre.com>
2024-03-29 11:59:17 -0400

    Initial

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-# Description: This file contains the code for Passivebot's Facebook Marketplace Scraper API.
+# Description: This file contains the code for Passivebot's x WordForest's Facebook Marketplace Scraper API. This code was initially authored by Harminder Nijjar and modified by Tanya Da Costa to fit custom Marketplace needs.
 # Date: 2024-01-24
-# Author: Harminder Nijjar
-# Version: 1.0.0.
+# Author: Harminder Nijjar, Tanya Da Costa
+# Version: 2.0.0.
 # Usage: python app.py
 
+# TODO: favicon
 
 # Import the necessary libraries.
 # Playwright is used to crawl the Facebook Marketplace.
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 # The os library is used to get the environment variables.
 import os
 # The time library is used to add a delay to the script.
@@ -47,64 +48,16 @@ def root():
     # Return a message.
     return {"message": "Welcome to Passivebot's Facebook Marketplace API. Documentation is currently being worked on along with the API. Some planned features currently in the pipeline are a ReactJS frontend, MongoDB database, and Google Authentication."}
 
-    # TODO - Add documentation to the API.
-    # TODO - Add a React frontend to the API.
-    # TODO - Add a MongoDB database to the API.
-    # TODO - Add Google Authentication to the React frontend.
-
 # Create a route to the return_data endpoint.
 @app.get("/crawl_facebook_marketplace")
 # Define a function to be executed when the endpoint is called.
 # Add a description to the function.
-def crawl_facebook_marketplace(city: str, query: str, max_price: int):
+# TODO: days since listed input
+def crawl_facebook_marketplace(city: str, query: str, max_price: int, max_results_per_query: int):
     # Define dictionary of cities from the facebook marketplace directory for United States.
     # https://m.facebook.com/marketplace/directory/US/?_se_imp=0oey5sMRMSl7wluQZ
-    # TODO - Add more cities to the dictionary.
     cities = {
-        'New York': 'nyc',
-        'Los Angeles': 'la',
-        'Las Vegas': 'vegas',
-        'Chicago': 'chicago',
-        'Houston': 'houston',
-        'San Antonio': 'sanantonio',
-        'Miami': 'miami',
-        'Orlando': 'orlando',
-        'San Diego': 'sandiego',
-        'Arlington': 'arlington',
-        'Balitmore': 'baltimore',
-        'Cincinnati': 'cincinnati',
-        'Denver': 'denver',
-        'Fort Worth': 'fortworth',
-        'Jacksonville': 'jacksonville',
-        'Memphis': 'memphis',
-        'Nashville': 'nashville',
-        'Philadelphia': 'philly',
-        'Portland': 'portland',
-        'San Jose': 'sanjose',
-        'Tucson': 'tucson',
-        'Atlanta': 'atlanta',
-        'Boston': 'boston',
-        'Columnbus': 'columbus',
-        'Detroit': 'detroit',
-        'Honolulu': 'honolulu',
-        'Kansas City': 'kansascity',
-        'New Orleans': 'neworleans',
-        'Phoenix': 'phoenix',
-        'Seattle': 'seattle',
-        'Washington DC': 'dc',
-        'Milwaukee': 'milwaukee',
-        'Sacremento': 'sac',
-        'Austin': 'austin',
-        'Charlotte': 'charlotte',
-        'Dallas': 'dallas',
-        'El Paso': 'elpaso',
-        'Indianapolis': 'indianapolis',
-        'Louisville': 'louisville',
-        'Minneapolis': 'minneapolis',
-        'Oaklahoma City' : 'oklahoma',
-        'Pittsburgh': 'pittsburgh',
-        'San Francisco': 'sanfrancisco',
-        'Tampa': 'tampa'
+        'Hamilton': 'hamilton'  # TODO: more oNTARIO cities
     }
     # If the city is in the cities dictionary...
     if city in cities:
@@ -117,76 +70,32 @@ def crawl_facebook_marketplace(city: str, query: str, max_price: int):
         city = city.capitalize()
         # Raise an HTTPException.
         raise HTTPException (404, f'{city} is not a city we are currently supporting on the Facebook Marketplace. Please reach out to us to add this city in our directory.')
-        # TODO - Try and find a way to get city location ids from Facebook if the city is not in the cities dictionary.
         
     # Define the URL to scrape.
-    marketplace_url = f'https://www.facebook.com/marketplace/{city}/search/?query={query}&maxPrice={max_price}'
-    initial_url = "https://www.facebook.com/login/device-based/regular/login/"
-    # Get listings of particular item in a particular city for a particular price.
-    # Initialize the session using Playwright.
-    with sync_playwright() as p:
-        # Open a new browser page.
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        # Navigate to the URL.
-        page.goto(initial_url)
-        # Wait for the page to load.
-        time.sleep(2)
-        try:
-            email_input = page.wait_for_selector('input[name="email"]').fill('YOUR_EMAIL_HERE')
-            password_input = page.wait_for_selector('input[name="pass"]').fill('YOUR_PASSWORD_HERE')
-            time.sleep(2)
-            login_button = page.wait_for_selector('button[name="login"]').click()
-            time.sleep(2)
-            page.goto(marketplace_url)
-        except:
-            page.goto(marketplace_url)
-        # Wait for the page to load.
-        time.sleep(2)
-        # Infinite scroll to the bottom of the page until the loop breaks.
-        # for _ in range(5):
-        #     page.keyboard.press('End')
-        #     time.sleep(2)
-        html = page.content()
-        soup = BeautifulSoup(html, 'html.parser')
-        parsed = []
-        listings = soup.find_all('div', class_='x9f619 x78zum5 x1r8uery xdt5ytf x1iyjqo2 xs83m0k x1e558r4 x150jy0e x1iorvi4 xjkvuk6 xnpuxes x291uyu x1uepa24')
-        for listing in listings:
-            try:
-                # Get the item image.
-                image = listing.find('img', class_='xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3')['src']
-                # Get the item title from span.
-                title = listing.find('span', 'x1lliihq x6ikm8r x10wlt62 x1n2onr6').text
-                # Get the item price.
-                price = listing.find('span', 'x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u').text
-                # Get the item URL.
-                post_url = listing.find('a', class_='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv')['href']
-                # Get the item location.
-                location = listing.find('span', 'x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84').text
-                # Append the parsed data to the list.
-                parsed.append({
-                    'image': image,
-                    'title': title,
-                    'price': price,
-                    'post_url': post_url,
-                    'location': location
-                })
-            except:
-                pass
-        # Close the browser.
-        browser.close()
-        # Return the parsed data as a JSON.
-        result = []
-        for item in parsed:
-            result.append({
-                'name': item['title'],
-                'price': item['price'],
-                'location': item['location'],
-                'title': item['title'],
-                'image': item['image'],
-                'link': item['post_url']
-            })
-        return result
+    results = []
+    # Split the query into a list
+    query_list = query.split(',')
+    for query in query_list:
+      recent_query_results = crawl_query(city, query, max_price, max_results_per_query, False)
+      suggested_results =  crawl_query(city, query, max_price, max_results_per_query, True)
+
+      print("suggested. do these show up in the results read out?")
+      print(suggested_results)
+
+      recent_query_results_urls = [item["link"] for item in recent_query_results]
+      suggested_results_urls = [item["link"] for item in suggested_results]
+
+      # If the items appear in both recent and suggested, put them in the top of the list
+      common_items = set(recent_query_results_urls) & set(suggested_results_urls)
+
+      consolidated_query_result_urls = list(common_items) + [item for item in recent_query_results_urls + suggested_results_urls if item not in list(common_items)]
+      consolidated_query_results = [item for item in recent_query_results + suggested_results if item["link"] in consolidated_query_result_urls]
+
+      print("consolidated") # TODO: FUCKING CONSolidated is missing suggested. oh.copy pasta bug
+      print(consolidated_query_results)
+      results.extend(consolidated_query_results)
+
+    return results
 
 # Create a route to the return_html endpoint.
 @app.get("/return_ip_information")
@@ -219,8 +128,6 @@ def return_ip_information():
         ip_type = soup.find('strong', id='ip_type').text
         # Find the version.
         version = soup.find('strong', id='version').text
-        # Close the browser.
-        browser.close()
         # Return the IP information as JSON.
         return {
             'ip_address': ip_address,
@@ -241,3 +148,68 @@ if __name__ == "__main__":
         host='127.0.0.1',
         port=8000
     )
+
+def crawl_query(city: str, query: str, max_price: int, max_results: int, suggested: bool):
+    # TODO: sometimes facebook asks us to login. need to get login back but with persisting session cookies. Or figure out how to close the modal (nvm we can still scrape with the modal open)
+    marketplace_url = f'https://www.facebook.com/marketplace/{city}/search?query={query}&maxPrice={max_price}&daysSinceListed=1&sortBy=creation_time_descend'
+    if suggested:
+      marketplace_url = f'https://www.facebook.com/marketplace/{city}/search?query={query}&maxPrice={max_price}&daysSinceListed=3'
+    # Get listings of particular item in a particular city for a particular price.
+    # Initialize the session using Playwright.
+    with sync_playwright() as p:
+      # Open a new browser page.
+      browser = p.chromium.launch(headless=True)
+      page = browser.new_page()
+      page.goto(marketplace_url) # TODO: crash is due to timeout here. try catch?
+
+      # Wait for the page to load.
+      time.sleep(5)
+      html = page.content()
+      soup = BeautifulSoup(html, 'html.parser')
+      parsed = []
+      listings = soup.find_all('div', class_='x9f619 x78zum5 x1r8uery xdt5ytf x1iyjqo2 xs83m0k x1e558r4 x150jy0e x1iorvi4 xjkvuk6 xnpuxes x291uyu x1uepa24')
+      # Grab the latest lists
+      latest_listings = listings[:max_results]
+
+      for listing in latest_listings:
+            # Get the item image.
+            image = listing.find('img', class_='xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3')
+            if image is not None:
+              image = image['src']
+
+            # TODO: better way to grab these or move these classes to config. They change sometimes
+            # Get the item title from span.
+            title = listing.find('span', 'x1lliihq x6ikm8r x10wlt62 x1n2onr6')
+            if title is not None:
+              title = title.text
+
+            # Get the item URL.
+            post_url = listing.find('a', class_='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv')
+            if post_url is not None:
+              post_url = post_url['href']
+            
+            if title is not None and post_url is not None and image is not None:
+              # Append the parsed data to the list.
+              parsed.append({
+                  'image': image,
+                  # 'location': location,
+                  'title': title,
+                  # 'price': price,
+                  'post_url': post_url
+              })
+
+      # Return the parsed data as a JSON.
+      # TODO: put in a dict for query headings
+      result = []
+      for item in parsed:
+          result.append({
+              'name': item['title'],
+              # 'price': item['price'],
+              # 'location': item['location'],
+              'title': item['title'],
+              'image': item['image'],
+              'link': item['post_url']
+          })
+
+      return result
+
--- gui.py
+++ gui.py
@@ -1,49 +1,243 @@
+import random
 import streamlit as st
+import schedule
+import streamlit.components.v1 as stcomponents
+import time
 import json 
 import requests
+from datetime import datetime
 from PIL import Image
 
+def countdown_timer():
+  countdown_message.empty()
+  duration = 5 * 60
+  while duration:
+        mins, secs = divmod(duration, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        countdown_message.text(f"Time until next auto scrape: {timeformat}")
+        time.sleep(1)
+        duration -= 1
+  countdown_message.text("Scraping...")
+
+
+def ding():
+  unique_id = f'dingSound_{time.time()}_{random.randint(1, 1000)}'
+  audio_html = f'<audio id="{unique_id}" autoplay><source src="app/static/ding.mp3"></audio>'
+  stcomponents.html(audio_html)
+
+def crawl():
+  global max_price
+  global city
+  global query
+
+  # Workaround to hide the ugly iframe that the new results alert component gets rendered in
+  st.markdown(
+    """
+    <style>
+        iframe {
+            display: none;  /* Hide the iframe */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+  )
+  
+  if "," in max_price:
+      max_price = max_price.replace(",", "")
+  elif "$" in max_price:
+      max_price = max_price.replace("$", "")
+  else:
+      pass
+  res = requests.get(f"http://127.0.0.1:8000/crawl_facebook_marketplace?city={city}&sortBy=creation_time_descend&query={query}&max_price={str(int(max_price) * 100)}&max_results_per_query={max_listings}")
+
+  # Convert the response from json into a Python list.
+  results = res.json()
+
+  # Display the length of the results list.
+  if len(results) > 0:
+    # Determine if there are new results and log an alert if so
+    latest = [json.dumps(item["title"]) for item in results] # TODO: index on url instead of title in case of dupes
+    diff = [item for item in latest if item not in st.session_state.current_latest]
+    if len(diff) > 0:
+      # Reset the latest list
+      st.session_state.current_latest = latest
+      latest_string = "\\n\\n".join(diff)
+      # TODO: still getting repeat alerts. perhaps when random login prompt?
+      alert_js=f"alert('New listings!!\\n\\n{latest_string}')"
+      alert_html = f"<script>{alert_js}</script>"
+
+      stcomponents.html(alert_html)
+      ding()
+
+  last_ran_formatted = datetime.now().time().strftime("%I:%M:%S %p")
+  results_message.markdown(f"*Showing latest {max_listings} listings (per query) since last scrape at **{last_ran_formatted}***")
+
+  # Clear previous results
+  results_container.empty()
+
+  # Iterate over the results list to display each item.
+  # TODO: new! badge. query headings
+  with results_container.container():
+    for item in results:
+        st.header(item["title"])
+        img_url = item["image"] # TODO: make the whole row clickable link to listing
+        st.image(img_url, width=200)
+        st.write(f"https://www.facebook.com{item['link']}")
+        st.write("----")
+
+  countdown_timer()
+
+# End of private functions
+
+# Initialize session state
+if 'current_latest' not in st.session_state:
+    st.session_state.current_latest = []
+
 # Create a title for the web app.
-st.title("Passivebot's Facebook Marketplace Scraper")
+st.title("DingBot™ Facebook Scraper")
+st.subheader("Brought to you by Passivebot + WordForest")
 
 # Add a list of supported cities.
-supported_cities = ["New York", "Los Angeles", "Las Vegas", "Chicago", "Houston", "San Antonio", "Miami", "Orlando", "San Diego", "Arlington", "Baltimore", "Cincinnati", "Denver", "Fort Worth", "Jacksonville", "Memphis", "Nashville", "Philadelphia", "Portland", "San Jose", "Tucson", "Atlanta", "Boston", "Columbus", "Detroit", "Honolulu", "Kansas City", "New Orleans", "Phoenix", "Seattle", "Washington DC", "Milwaukee", "Sacramento", "Austin", "Charlotte", "Dallas", "El Paso", "Indianapolis", "Louisville", "Minneapolis", "Oklahoma City", "Pittsburgh", "San Francisco", "Tampa"]
+supported_cities = ["Hamilton"] # TODO: more oNTARIO cities
 
 # Take user input for the city, query, and max price.
 city = st.selectbox("City", supported_cities, 0)
-query = st.text_input("Query", "Macbook Pro")
-max_price = st.text_input("Max Price", "1000")
+query = st.text_input("Query (comma,between,multiple,queries)", "VHS,Digimon,playstation 1")
+max_price = st.text_input("Max Price ($)", "1000")
+# This value should be calibrated to your queries. Facebook sometimes is very lax about what they think
+# is related to your search query.
+max_listings = st.text_input("Max Latest Listings", "8")
+# TODO: auto scrape every 3, 5, 10, 30 minutes select
 
-# Create a button to submit the form.
-submit = st.button("Submit")
+countdown_message = st.empty()
 
-# If the button is clicked.
+# TODO: shouldn't clear results
+submit = st.button("Force Scrape Now!")
+
+results_message = st.empty()
+results_container = st.empty()
+
+# If the button is clicked
 if submit:
-    # TODO - Remove any commas from the max_price before sending the request.
-    if "," in max_price:
-        max_price = max_price.replace(",", "")
-    else:
-        pass
-    res = requests.get(f"http://127.0.0.1:8000/crawl_facebook_marketplace?city={city}&query={query}&max_price={max_price}"
-    )
-    
-    # Convert the response from json into a Python list.
-    results = res.json()
-    
-    # Display the length of the results list.
-    st.write(f"Number of results: {len(results)}")
-    
-    # Iterate over the results list to display each item.
-    for item in results:
-        st.header(item["title"])
-        img_url = item["image"]
-        st.image(img_url, width=200)
-        st.write(item["price"])
-        st.write(item["location"])
-        st.write(f"https://www.facebook.com{item['link']}")
-        st.write("----")
-    
+  countdown_message.text("Scraping...")
+  crawl()
 
-      
+# Schedule the scraper to run every 5 minutes # TODO: interval to variable
+schedule.every(5).minutes.do(crawl)
+# Timer message
+countdown_timer() # TODO: FIRST auto scrape not working?
+
+# Run the scheduler
+while True:
+  schedule.run_pending()
+  time.sleep(2)  # Sleep for 1 second to avoid high CPU usage
 
 
+
+# TODO: crash randomly at night. on sleep? could it be due to facebook showing the "login to see more " modal?
+# 2024-01-17 23:49:25.548 Uncaught app exception
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/requests/models.py", line 971, in json
+#     return complexjson.loads(self.text, **kwargs)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/__init__.py", line 346, in loads
+#     return _default_decoder.decode(s)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/decoder.py", line 337, in decode
+#     obj, end = self.raw_decode(s, idx=_w(s, 0).end())
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/decoder.py", line 355, in raw_decode
+#     raise JSONDecodeError("Expecting value", s, err.value) from None
+# json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 534, in _run_script
+#     exec(code, module.__dict__)
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 89, in <module>
+#     while True:
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 822, in run_pending
+#     default_scheduler.run_pending()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 100, in run_pending
+#     self._run_job(job)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 172, in _run_job
+#     ret = job.run()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 693, in run
+#     ret = self.job_func()
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 41, in crawl
+#     results = res.json()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/requests/models.py", line 975, in json
+#     raise RequestsJSONDecodeError(e.msg, e.doc, e.pos)
+# requests.exceptions.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+# 2024-01-17 23:49:25.551 Uncaught app exception
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/requests/models.py", line 971, in json
+#     return complexjson.loads(self.text, **kwargs)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/__init__.py", line 346, in loads
+#     return _default_decoder.decode(s)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/decoder.py", line 337, in decode
+#     obj, end = self.raw_decode(s, idx=_w(s, 0).end())
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/json/decoder.py", line 355, in raw_decode
+#     raise JSONDecodeError("Expecting value", s, err.value) from None
+# json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 534, in _run_script
+#     exec(code, module.__dict__)
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 89, in <module>
+#     while True:
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 822, in run_pending
+#     default_scheduler.run_pending()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 100, in run_pending
+#     self._run_job(job)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 172, in _run_job
+#     ret = job.run()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 693, in run
+#     ret = self.job_func()
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 41, in crawl
+#     results = res.json()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/requests/models.py", line 975, in json
+#     raise RequestsJSONDecodeError(e.msg, e.doc, e.pos)
+# requests.exceptions.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+# DVD/VHS Player $15 (o.b.o)
+# []
+# Digimon Omegamon Pure X-Body Model Kit Built
+# []
+# |^\  Stopping...
+# ^\  Stopping...
+# Exception ignored in: <module 'threading' from '/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/threading.py'>
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/threading.py", line 1448, in _shutdown
+#     lock.acquire()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/web/bootstrap.py", line 69, in signal_handler
+#     server.stop()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/web/server/server.py", line 397, in stop
+#     self._runtime.stop()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/runtime/runtime.py", line 308, in stop
+#     async_objs.eventloop.call_soon_threadsafe(stop_on_eventloop)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/asyncio/base_events.py", line 791, in call_soon_threadsafe
+#     self._check_closed()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/asyncio/base_events.py", line 510, in _check_closed
+#     raise RuntimeError('Event loop is closed')
+# RuntimeError: Event loop is closed
+# ➜  facebook-marketplace-scraper git:(main) ✗ streamlit run gui.py
+
+
+# TODO: another crash
+# []
+# 2024-01-23 21:40:27.516 Uncaught app exception
+# Traceback (most recent call last):
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 534, in _run_script
+#     exec(code, module.__dict__)
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 130, in <module>
+#     schedule.run_pending()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 822, in run_pending
+#     default_scheduler.run_pending()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 100, in run_pending
+#     self._run_job(job)
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 172, in _run_job
+#     ret = job.run()
+#   File "/Users/hifyre/.pyenv/versions/3.9.6/lib/python3.9/site-packages/schedule/__init__.py", line 693, in run
+#     ret = self.job_func()
+#   File "/Users/hifyre/Codes/facebook-marketplace-scraper/gui.py", line 91, in crawl
+# AttributeError: '_GeneratorContextManager' object has no attribute 'empty'
